2a8b4521c10b7881fcdb0dc4461616805de2ad09
jojotenya <jojotenya@gmail.com>
2018-03-01 16:33:42 +0800

    FileID被各個ProductID開啟的次數的比例

---
+++ util.py
@@ -0,0 +1,9 @@
+#FileID被各個ProductID開啟的次數的比例
+def get_file_product_count_percentage(df):
+    dft = df[['FileID','ProductID']]
+    dft = dft.assign(Count=1)
+    dft = dft.groupby(['FileID','ProductID'],as_index = False).sum().pivot('FileID','ProductID').fillna(0)
+    dft = dft.div(dft.sum(axis=1), axis=0)
+    cols = [col+'_count_percentage' for col in list(dft.columns.get_level_values(1))]
+    dft.columns = cols
+    return dft
