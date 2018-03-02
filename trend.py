aa1cff6082d643d39707ab35ff1c55da8d5b27e7
jojotenya <jojotenya@gmail.com>
2018-03-02 12:19:44 +0000

    v1

--- trend.py
+++ trend.py
@@ -7,7 +7,9 @@ x_all = x_all.fillna(-1)
 #best_gmm = best_gmm_cluster(x_all)
 #joblib.dump(best_gmm,'export/trend_best_gmm.pkl')
 best_gmm = joblib.load('trend_best_gmm.pkl')
-x_all = best_gmm.fit(x_all)
+x_all = best_gmm.predict_proba(x_all)
+x = x_all[:x.shape[0]]
+test = x_all[:test.shape[0]]
 
 
 lr = LogisticRegression(C=0.1)
@@ -17,14 +19,13 @@ lgbm = LGBMClassifier(n_estimators=n_estimators,max_depth=max_depth)
 rdf = RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth)
 classifiers = [rdf,xgbm,lgbm]
 grid = StackingClassifier(classifiers=classifiers, 
-                           meta_classifiers=lr)
+                           meta_classifier=lr)
 
 params = {'xgbclassifier__n_estimators': [100],
           'xgbclassifier__max_depth': [3],
           'lgbmclassifier__n_estimators':[100],
           'lgbmclassifier__max_depth': [3],
           #'lasso__alpha': [0.01,0.1,0.5],
-          'lasso__alpha': [0.1],
           'randomforestclassifier__n_estimators':[100],
           'randomforestclassifier__max_depth': [3],
           'meta-logisticregression__C': [0.1]
@@ -36,13 +37,12 @@ grid = GridSearchCV(estimator=grid,
                     refit=True)
 
 print('fitting')
-print(x.isnull().values.any())
-print(y.isnull().values.any())
 grid.fit(x,y)
 
 #joblib.dump(grid, 'export/trend_model.pkl') 
 
 predicted = grid.predict_proba(x)
+predicted = list(map(lambda x:x[1],predicted))
 print('trian roc: ',roc_auc_score(y,predicted))
 
 predicted = pd.Series(grid.predict_proba(test))
