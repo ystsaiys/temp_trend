094d5ab7b77674b8e49199ecd8548017df9d4fce
jojotenya <jojotenya@gmail.com>
2018-03-04 09:13:25 +0000

    gridsearch

--- trend.py
+++ trend.py
@@ -1,4 +1,13 @@
-from util import *
+from util import get_data
+from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
+import pandas as pd
+from mlxtend.classifier import StackingClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 x,y,test = get_data()
 train_ids = x.index
 test_ids = test.index
@@ -6,45 +15,46 @@ x_all = pd.concat([x,test],axis=0)
 x_all = x_all.fillna(-1)
 #best_gmm = best_gmm_cluster(x_all)
 #joblib.dump(best_gmm,'export/trend_best_gmm.pkl')
-best_gmm = joblib.load('trend_best_gmm.pkl')
-x_all = best_gmm.predict_proba(x_all)
+#best_gmm = joblib.load('trend_best_gmm.pkl')
+#x_all = best_gmm.predict_proba(x_all)
 x = x_all[:x.shape[0]]
 test = x_all[:test.shape[0]]
 
 
 lr = LogisticRegression(C=0.1)
-n_estimators,max_depth = 1500,3
-xgbm = XGBClassifier(n_estimators=n_estimators,max_depth=max_depth)
-lgbm = LGBMClassifier(n_estimators=n_estimators,max_depth=max_depth)
-rdf = RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth)
+xgbm = XGBClassifier(learning_rate=0.1,reg_alpha=0.1, reg_lambda=1)
+lgbm = LGBMClassifier(learning_rate=0.1,reg_alpha=0.1, reg_lambda=1)
+rdf = RandomForestClassifier()
 classifiers = [rdf,xgbm,lgbm]
-grid = StackingClassifier(classifiers=classifiers, 
-                           meta_classifier=lr)
+classifiers = [xgbm,lgbm]
+grid = StackingClassifier(classifiers=classifiers,use_probas=True,average_probas=False,meta_classifier=lr)
 
 params = {'xgbclassifier__n_estimators': [100],
           'xgbclassifier__max_depth': [3],
           'lgbmclassifier__n_estimators':[100],
           'lgbmclassifier__max_depth': [3],
-          #'lasso__alpha': [0.01,0.1,0.5],
-          'randomforestclassifier__n_estimators':[100],
-          'randomforestclassifier__max_depth': [3],
+          #'randomforestclassifier__n_estimators':[100],
+          #'randomforestclassifier__max_depth': [3],
           'meta-logisticregression__C': [0.1]
           }
 
-grid = GridSearchCV(estimator=grid, 
-                    param_grid=params, 
+grid = GridSearchCV(estimator=grid,
+                    param_grid=params,
                     cv=5,
-                    refit=True)
+                    refit=True,
+                    verbose=3,
+                    scoring='roc_auc')
 
 print('fitting')
 grid.fit(x,y)
 
-#joblib.dump(grid, 'export/trend_model.pkl') 
+joblib.dump(grid, 'trend_model_tmp.pkl') 
+
 
 predicted = grid.predict_proba(x)
 predicted = list(map(lambda x:x[1],predicted))
 print('trian roc: ',roc_auc_score(y,predicted))
 
-predicted = pd.Series(grid.predict_proba(test))
-result = pd.concat([ids,predicted],axis=1)
-result.to_csv('trend_predict_test.csv',index=False)
+predicted = pd.Series(grid.predict_proba(test)[:,1])
+predicted.index = test_ids
+predicted.to_csv('trend_predict_test.csv')
